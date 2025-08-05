e9144b47a9d9a1d888b805be203d4b07d7c299e1
ABHI9960DEO <abhishek.virgo0001@gmail.com>
2025-08-05 13:03:51 +0530

    first commit

---
+++ src/main/java/com/videostreamingbackend/VideostreamingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.videostreamingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostreamingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideostreamingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/videostreamingbackend/VideostreamingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videostreamingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostreamingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
